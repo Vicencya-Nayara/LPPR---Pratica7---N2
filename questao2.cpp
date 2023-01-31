6e7835c5b3483f0c192318e18271d56e83282c52
Vicencya-Nayara <115300537+Vicencya-Nayara@users.noreply.github.com>
2023-01-31 15:16:38 -0300

    Update questao2.cpp

--- questao2.cpp
+++ questao2.cpp
@@ -2,20 +2,27 @@
 #include <stdlib.h>
 
 int main(){
-	int t = 0;
-	char frase[500],  letra[1];
 	
+	char frase[500];
+	char letra;
+	int t = 0;
+	
+	printf("Digite sua frase: ");
 	gets(frase);
-	gets(letra);
+	
+	printf("Digite uma letra: ");
+	scanf("%c", &letra);
 	
 	for(int i = 0; i < 500; i++){
-		if(frase[i] == letra[0]){
+		if(frase[i] == letra){
 			t++;
 		}
 		if(frase[i] == '\0'){
 			break;
 		}
 	}
-	printf("A vogal %s aparece %d vezes", letra, t);
+	
+	printf("A vogal %c aparece %d vezes",letra, t);
+	
 	return 0;
 }
